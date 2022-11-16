e67da7a8259ffb6d301927a35adfc68d153fb3b6
federerkristijan <federer.kristijan@gmail.com>
2022-11-16 16:30:29 +0100

    needs reset

--- src/views/Aero.jsx
+++ src/views/Aero.jsx
@@ -5,7 +5,7 @@ import Air from "../assets/images/gas_giant.png";
 const Aero = () => {
   return (
     <div className="Aero">
-      <img src={Air} alt="aero" style={{ width: "80px", height: "80px", marginBottom: "2.5rem" }} />
+      <img src={Air} alt="aero" style={{ width: "80px", height: "80px", marginBottom: "2.5rem", position: "relative", right: "190px", bottom: "250px" }} />
     </div>
   );
 };
--- src/views/Hydro.jsx
+++ src/views/Hydro.jsx
@@ -1,13 +1,24 @@
-import React from 'react'
+import React from "react";
 
-import Water from "../assets/images/water_planet.png"
+import Water from "../assets/images/water_planet.png";
 
 const Hydro = () => {
   return (
-    <div className='Hydro'>
-      <img src={Water} alt="water" style={{ width: "80px", height: "80px", marginBottom: "3.5rem" }}/>
+    <div className="Hydro">
+      <img
+        src={Water}
+        alt="water"
+        style={{
+          width: "80px",
+          height: "80px",
+          marginBottom: "3.5rem",
+          position: "relative",
+          left: "320px",
+          top: "20px"
+        }}
+      />
     </div>
-  )
-}
+  );
+};
 
-export default Hydro
+export default Hydro;
