c8340fb1985567445fe29f673de6f2153104d20a
federerkristijan <federer.kristijan@gmail.com>
2022-11-24 11:05:51 +0100

    animatecursor removed. icons added. need background and cursor

--- src/App.js
+++ src/App.js
@@ -1,5 +1,10 @@
 import React from "react";
-import { BrowserRouter as Router, Routes, Route, Outlet } from "react-router-dom";
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  Outlet,
+} from "react-router-dom";
 
 import Home from "./views/Home";
 import "./App.css";
--- src/views/Home.jsx
+++ src/views/Home.jsx
@@ -1,9 +1,31 @@
 import React from 'react'
+// import AnimatedCursor from '../components/AnimatedCursor/AnimatedCursor';
+
+import Air from "../assets/Icons/Airplane";
+import Car from "../assets/Icons/Car";
+import DNA from "../assets/Icons/DNA_1.png";
+import Ship from "../assets/Icons/ship.png";
+import Wind from "../assets/Icons/wind.png";
 
 const Home = () => {
 
   return (
     <div className="Home">
+      <div className="air">
+        <img src={Air} alt='air'/>
+      </div>
+      <div className="car">
+        <img src={Car} alt='car'/>
+      </div>
+      <div className="DNA">
+        <img src={DNA} alt='DNA'/>
+      </div>
+      <div className="ship">
+        <img src={Ship} alt='ship'/>
+      </div>
+      <div className="wind">
+        <img src={Wind} alt='wind'/>
+      </div>
     </div>
   );
 };
